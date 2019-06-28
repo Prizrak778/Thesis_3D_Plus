1ad6bec8a9ef22ac0501659f7216862f837c77df
Владимир Меркель <v.merkel778@gmail.com>
2019-06-28 22:29:09 +0700

    Добавлены функции управления камерой

--- Thesis_3D_Plus/Thesis_3D_Plus/Camera.cpp
+++ Thesis_3D_Plus/Thesis_3D_Plus/Camera.cpp
@@ -2,6 +2,41 @@
 
 
 
+glm::mat4x4 Camera::GetViewMatrix()
+{
+	glm::vec3 lookat;
+	lookat.x = sinf(Orientation.x)*cosf(Orientation.y);
+	lookat.x = sinf(Orientation.y);
+	lookat.x = cosf(Orientation.x)*cosf(Orientation.y);
+	return glm::lookAt(Position, Position + lookat, glm::vec3(0, 1, 0));
+}
+
+void Camera::Move(float x, float y, float z)
+{
+	glm::vec3 offset = glm::vec3(0, 0, 0);
+
+	glm::vec3 forward = glm::vec3(sinf(Orientation.x), 0 ,cosf(Orientation.x));
+	glm::vec3 right = glm::vec3(forward.z, 0, forward.x);
+
+	offset += x * right;
+	offset += y * forward;
+	offset.y += z;
+
+	glm::normalize(offset);
+	offset = offset * MouseSensitivity;
+
+	Position += offset;
+}
+
+void Camera::AddRotation(float x, float y)
+{
+	x = x * MouseSensitivity;
+	y = y * MouseSensitivity;
+	float _y = (float)M_PI * 2.0f;
+	Orientation.x = modff((Orientation.x + x), &_y);
+	Orientation.y = glm::max(glm::min(Orientation.y + y, (float)M_PI / 2.0f - 0.1f), (float)-M_PI / 2.0f + 0.1f);
+}
+
 Camera::Camera()
 {
 }
--- Thesis_3D_Plus/Thesis_3D_Plus/Camera.h
+++ Thesis_3D_Plus/Thesis_3D_Plus/Camera.h
@@ -1,19 +1,25 @@
 #pragma once
 #define _USE_MATH_DEFINES
-#include <math.h>
+#include <cmath>
+#include <glm\vec3.hpp>
+#include <glm\mat4x4.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\matrix_transform.inl>
 #include <GL/glew.h>
 #include <GLFW\glfw3.h>
 #include <GLFW\glfw3native.h>
 class Camera
 {
 private:
-	
-	GLfloat Position[3] = {0.0f, 0.0f, 0.0f};
-	GLfloat Orientation[3] = { (float)M_PI, 0.0f, 0.0f };
+	glm::vec3 Position = { 0.0f, 0.0f, 0.0f };
+	glm::vec3 Orientation = { (float)M_PI, 0.0f, 0.0f };
 	float MoveSpeed = 0.05f;
 	float MouseSensitivity = 0.01f;
 	bool Rotation_status = true;
 public:
+	glm::mat4x4 GetViewMatrix();
+	void Move(float x, float y, float z);
+	void AddRotation(float x, float y);
 	Camera();
 	~Camera();
 };
